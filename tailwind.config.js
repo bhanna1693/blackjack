59164ef18930cdf635a5ae11d431d120ed56007f
brianhanna <bhanna1693@gmail.com>
2024-08-03 10:38:01 -0400

    added daisyUI

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+import daisyui from 'daisyui'
+
+// eslint-disable-next-line no-undef
+module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {}
   },
-  plugins: []
+  plugins: [daisyui]
 }
