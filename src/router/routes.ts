7b285b3a22bce64b6f8b188959b18d0c1895e89f
brianhanna <bhanna1693@gmail.com>
2024-08-04 14:44:02 -0400

    removed /about route

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -11,13 +11,13 @@ export const routes: RouteRecordRaw[] = [
     path: '/blackjack',
     name: 'Blackjack',
     component: () => import('../views/BlackjackView.vue')
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (About.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import('../views/AboutView.vue')
   }
+  // {
+  //   path: '/about',
+  //   name: 'About',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (About.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () => import('../views/AboutView.vue')
+  // }
 ]
